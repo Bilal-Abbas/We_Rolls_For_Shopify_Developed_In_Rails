f29e6613166c1f4631dfaaee0ec6aaf1f2ab4d62
BilalAbbasAwan <bilalabbas437@gmail.com>
2018-12-22 15:21:41 +0500

    applclear

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :shopify_app_settings
   
-  def shopify_app_settings
-  	shop_url = "https://0836388da714550d1c6a8f51bf848be8:65eaa1e76d3000b06dc38621ef8636df@coordinates-collection.myshopify.com/admin"
-  	ShopifyAPI::Base.site = shop_url
-  	@shopify_shop = ShopifyAPI::Shop.current
-  	@shop = Shop.validate_shop(@shopify_shop)
-  end
-
-  private
-
-
-  def after_sign_out_path_for(resource_or_scope)
-  	ShopifyAPI::Base.clear_session
-  end
 end
